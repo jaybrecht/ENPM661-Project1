798bcee4a617fa7b06cb6308afdbb53f72b2586b
Justin Albrecht <jalbrec1@umd.edu>
2020-02-10 11:08:35 -0500

    write BlankTileLocation and modified print_matrix to print blank space instead of 0

--- functions.py
+++ functions.py
@@ -1,11 +1,22 @@
-def print_matrix(state):
+def print_matrix(node):
     counter = 0
-    for row in range(0, len(state)-1, 3):
+    for row in range(0, len(node)-1, 3):
         if counter == 0 :
             print("-------------")
-        for element in range(counter, len(state)-1, 3):
+        for element in range(counter, len(node)-1, 3):
             if element <= counter:
                 print("|", end=" ")
-            print(int(state[element]), "|", end=" ")
+            if(node[element] == 0):
+                print(" ", "|", end=" ")
+            else:
+                print(int(node[element]), "|", end=" ")
         counter = counter +1
         print("\n-------------")
+
+def BlankTileLocation(node):
+    # finds the index of the blank element in the list
+    ind = node.index(0)
+    # Converts index in list to column (i) and row (j)
+    i = (ind//3) + 1
+    j = (ind%3) + 1
+    return [i,j]
