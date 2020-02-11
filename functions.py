d71529b3bb788655e617b1b7fccc66080ca9d74d
Justin Albrecht <jalbrec1@umd.edu>
2020-02-11 14:15:31 -0500

    Fixed bugs, moved BFS and game mode into functions

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def BlankTileLocation(node):
     # finds the index of the blank element in the list
     ind = node.index(0)
@@ -114,9 +116,79 @@ def check_neighbors(cur_node,direction):
     return [status,new_node]
 
 
+def BFS(start_node,goal_node):
+    # Data structure for the nodes is a list of lists, each list inside the list 
+    # nodes is a node. The first 3 elements are the first column, the second 3 
+    # elements are the second column, and the next 3 are the last column. The last 
+    # element in the list is the parent node of the node and refers to the index in 
+    # nodes where it can be found. The starting node has a parent of -1
+    nodes = [[]]
+
+    # Checked nodes are additionally stored in a set which is much faster for 
+    # checking if the node has already been visited
+    node_set = {tuple(start_node)}
+
+    start_node.append(-1) #set start node to have parent of -1
+    nodes[0] = start_node
+
+    # The queue is strucuted as a deque which allows for much faster operation
+    # when accessing the first item in the list
+    queue = deque()
+    queue.append(0) #set the start_node as the first node in the queue
+
+    isgoal = False
+    success = False
+
+    neighbors = ['left','up','right','down'] #modify order to change the order of search
+    while queue:
+        # Set the current node as the top of the queue and remove it
+        parent = queue.popleft();
+        cur_node = nodes[parent]
+        for direction in neighbors:
+            [status,new_node] = check_neighbors(cur_node,direction)
+            if status == True:
+                new_node[9] = parent
+                [nodes,node_set,exists] = AddNode(new_node,nodes,node_set)
+                if not exists:            
+                    queue.append(len(nodes)-1)
+            if new_node[0:9] == goal_node:
+                isgoal = True
+                break
+        if isgoal:
+            success = True
+            break
+
+    return [nodes,success]
 
     
+def startGame():
+    print("\n*****************************")
+    print("Welcome to the 8 Puzzle Game")
+    print("*****************************\n")
+    print("Please enter the starting configuration of your puzzle")
+    print("  It should have the form of \n  11,21,31,12,22,32,13,23,33\n")
+    print("  For a puzzle like this:\n")
+    print("    11  12  13\n")
+    print("    21  22  23\n")
+    print("    31  32  33\n")
 
-    
-   
+    start_node_str = input("Starting Configuration: ")
 
+    print("\nPlease enter the goal configuration of your puzzle")
+    goal_node_str = input("\nGoal Configuration: ")
+
+    start_node = [] #[8,4,2,3,1,7,5,6,0]
+    goal_node = [] #[5,4,3,6,0,2,7,8,1]
+
+    for s in start_node_str:
+        if s.isdigit():
+            start_node.append(int(s))
+
+    for s in goal_node_str:
+        if s.isdigit():
+            goal_node.append(int(s))
+
+    if (len(goal_node) != 9) or (len(start_node) != 9):
+        raise Exception('Your configurations are not formatted correctly')
+
+    return [start_node,goal_node]
--- main.py
+++ main.py
@@ -1,81 +1,34 @@
 from functions import *
 from plot_path import *
-from collections import deque
 
-# Data structure for the nodes is a list of lists, each list inside the list 
-# nodes is a node. The first 3 elements are the first column, the second 3 
-# elements are the second column, and the next 3 are the last column. The last 
-# element in the list is the parent node of the node and refers to the index in 
-# nodes where it can be found. The starting node has a parent node of -1
+game_mode = True
 
-nodes = [[]]
+if game_mode:
+    [start_node,goal_node] = startGame()
+else:
+    # Possible Case
+    # start_node = [8,4,2,3,1,7,5,6,0]
+    # goal_node = [5,4,3,6,0,2,7,8,1]
 
-print("\n*****************************")
-print("Welcome to the 8 Puzzle Game")
-print("*****************************\n")
-print("Please enter the starting configuration of your puzzle")
-print("  It should have the form of \n  11,21,31,12,22,32,13,23,33\n")
-print("  For a puzzle like this:\n")
-print("    11  12  13\n")
-print("    21  22  23\n")
-print("    31  32  33\n")
+    # Impossible Case
+    start_node = [8,0,7,1,4,6,2,3,5]
+    goal_node = [1,4,7,2,5,8,3,6,0]
 
-start_node_str = input("Starting Configuration: ")
+[nodes,success] = BFS(start_node,goal_node)
 
-print("Please enter the goal configuration of your puzzle")
-goal_node_str = input("Goal Configuration: ")
+if not success:
+    print("\nThe goal configuration cannot be reached.")
+    print("Please try a different configuration\n")
 
-start_node = [] #[8,4,2,3,1,7,5,6,0]
-goal_node = [] #[5,4,3,6,0,2,7,8,1]
+else:
+    path = generate_path(nodes)
 
-for s in start_node_str:
-    if s.isdigit():
-        start_node.append(int(s))
+    path_file = open("nodePath.txt","w+")
+    for node in path:
+        for tile in node:
+            path_file.write(str(tile)+" ")
+        path_file.write("\n")
+    path_file.close()
 
-for s in goal_node_str:
-    if s.isdigit():
-        goal_node.append(int(s))
+    plot_path()
 
-if (len(goal_node) != 9) or (len(start_node) != 9):
-    raise Exception('Your configurations are not formatted correctly')
-
-# Checked nodes are additionally stored in a set which is much faster for 
-# checking if the node has already been visited
-node_set = {tuple(start_node)}
-
-start_node.append(-1)
-nodes[0] = start_node
-
-# The queue is strucuted as a deque which allows for much faster operation
-queue = deque()
-queue.append(0)
-
-isgoal = False
-neighbors = ['left','up','right','down'] #modify order to change the order of search
-while queue:
-    # Set the current node as the top of the queue and remove it
-    cur_node = nodes[queue.popleft()]
-    parent = nodes.index(cur_node)
-    for direction in neighbors:
-        [status,new_node] = check_neighbors(cur_node,direction)
-        if status == True:
-            new_node[9] = parent
-            [nodes,node_set,exists] = AddNode(new_node,nodes,node_set)
-            if not exists:            
-                queue.append(len(nodes))
-        if new_node[0:9] == goal_node:
-            isgoal = True
-            break
-    if isgoal:
-        break
-
-path = generate_path(nodes)
-
-path_file = open("nodePath.txt","w+")
-for node in path:
-    for tile in node:
-        path_file.write(str(tile)+" ")
-    path_file.write("\n")
-path_file.close()
-
-plot_path()
