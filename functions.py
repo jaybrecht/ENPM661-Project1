22f433d5763979e4a06721f9ab2527d860421a1b
Justin Albrecht <jalbrec1@umd.edu>
2020-02-10 22:25:58 -0500

    implemented plot_path and set up main to run as a user-input game in terminal

--- functions.py
+++ functions.py
@@ -1,19 +1,3 @@
-def print_matrix(node):
-    counter = 0
-    for row in range(0, len(node)-1, 3):
-        if counter == 0 :
-            print("-------------")
-        for element in range(counter, len(node)-1, 3):
-            if element <= counter:
-                print("|", end=" ")
-            if(node[element] == 0):
-                print(" ", "|", end=" ")
-            else:
-                print(int(node[element]), "|", end=" ")
-        counter = counter +1
-        print("\n-------------")
-
-
 def BlankTileLocation(node):
     # finds the index of the blank element in the list
     ind = node.index(0)
@@ -109,12 +93,12 @@ def generate_path(nodes):
         parent_node = nodes[path_nodes[-1]]
         parent = parent_node[9]
         path_nodes.append(parent)
-    path = [nodes[-1]]
+    path = [nodes[-1][0:9]]
     for ind in path_nodes:
         if ind == -1:
             break
         else:
-            path.insert(0,nodes[ind])
+            path.insert(0,nodes[ind][0:9])
     return path
 
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functions import *
+from plot_path import *
 from collections import deque
 
 # Data structure for the nodes is a list of lists, each list inside the list 
@@ -9,20 +10,50 @@ from collections import deque
 
 nodes = [[]]
 
-start_node = [8,4,2,3,1,7,5,6,0]
-goal_node = [5,4,3,6,0,2,7,8,1]
+print("\n*****************************")
+print("Welcome to the 8 Puzzle Game")
+print("*****************************\n")
+print("Please enter the starting configuration of your puzzle")
+print("  It should have the form of \n  11,21,31,12,22,32,13,23,33\n")
+print("  For a puzzle like this:\n")
+print("    11  12  13\n")
+print("    21  22  23\n")
+print("    31  32  33\n")
 
+start_node_str = input("Starting Configuration: ")
+
+print("Please enter the goal configuration of your puzzle")
+goal_node_str = input("Goal Configuration: ")
+
+start_node = [] #[8,4,2,3,1,7,5,6,0]
+goal_node = [] #[5,4,3,6,0,2,7,8,1]
+
+for s in start_node_str:
+    if s.isdigit():
+        start_node.append(int(s))
+
+for s in goal_node_str:
+    if s.isdigit():
+        goal_node.append(int(s))
+
+if (len(goal_node) != 9) or (len(start_node) != 9):
+    raise Exception('Your configurations are not formatted correctly')
+
+# Checked nodes are additionally stored in a set which is much faster for 
+# checking if the node has already been visited
 node_set = {tuple(start_node)}
 
 start_node.append(-1)
 nodes[0] = start_node
 
+# The queue is strucuted as a deque which allows for much faster operation
 queue = deque()
 queue.append(0)
 
 isgoal = False
-neighbors = ['right','left','up','down'] #modify order to change the order of search
+neighbors = ['left','up','right','down'] #modify order to change the order of search
 while queue:
+    # Set the current node as the top of the queue and remove it
     cur_node = nodes[queue.popleft()]
     parent = nodes.index(cur_node)
     for direction in neighbors:
@@ -39,8 +70,12 @@ while queue:
         break
 
 path = generate_path(nodes)
+
+path_file = open("nodePath.txt","w+")
 for node in path:
-    print_matrix(node)
+    for tile in node:
+        path_file.write(str(tile)+" ")
+    path_file.write("\n")
+path_file.close()
 
-print("The goal state can be achieved with ",len(path)-1, " steps.")
-
+plot_path()
